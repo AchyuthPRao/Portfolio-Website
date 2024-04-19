92b2013ec23df7428f9a5b5cb44f6b60a3d55cfa
AchyuthPRao <achyuthprao123@gmail.com>
2024-04-19 16:17:48 +0530

    Nokia Added

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,18 @@ export default function About() {
         <h1 className="text-3xl sm:text-5xl font-bold text-white mx-4 ">
           About
         </h1>
-        <p className="text-white m-4 text-sm sm:text-2xl leading-7">
-          I'm a skilled software developer with experience in MERN Stack Web Dev.I collaborate with clients to create
-          efficient, scalable, and user-friendly solutions that solve real-world
-          problems. Let's work together to bring your ideas to life!
+        <p className="text-white m-4 text-sm sm:text-2xl leading-7 sm:leading-10 ">
+          I am currently pursuing my studies at Dayananda Sagar College of
+          Engineering in Bangalore, specializing in <strong className="font-bold tracking-tight text-gray-900 bg-gradient-to-r from-pink-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient">Full Stack Web
+          Development and Machine Learning.</strong>  My academic journey has equipped me
+          with a strong foundation in these areas, allowing me to build
+          practical solutions and contribute to real-world projects. During my
+          time at college, I have had the opportunity to<strong className="font-bold tracking-tight text-gray-900 bg-gradient-to-r from-pink-700 via-blue-500 to-green-400 text-transparent bg-clip-text bg-300% animate-gradient"> develop multiple
+          websites for various college events and Hackathons,</strong> showcasing my Full Stack Web
+          Development skills. Additionally, I have taken on <strong className="font-bold ">Freelance projects </strong> 
+          where I have designed and implemented websites tailored to clients'
+          specific needs.I'm passionate about using technology to solve challenges and always eager to learn and adapt to the latest trends.<strong className="font-bold text-blue-300">Let's collaborate and build something impactful
+          together!</strong>
         </p>
       </div>
     </>
